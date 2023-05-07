1d9eddf59ffa51700757bc42bbf3c11a64df7718
BACH VAN THANH HUY <67851389+ThanhHuy999@users.noreply.github.com>
2023-05-07 16:53:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+// connect to a database 
+$dbConn = pg_connect("host=<ec2-44-213-228-107.compute-1.amazonaws.com> port=<5432> dbname=<duge63lregjm7> user=<tsfdnzfevzaigp> password=<98638e683f0711f3d801aeb83587b7ecbe33617836980f9eb641d38442598395>");
+if (!$dbConn) {
+    echo "An error occurred.\n";
+    exit;
+}
+// Query data
+$result = pg_query($dbConn, 'SELECT TieuDe FROM BAI_BAO WHERE MaSoBaiBao='1';);
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+// Show value
+while ($row = pg_fetch_assoc($result)) {
+    var_dump($row);
+}
