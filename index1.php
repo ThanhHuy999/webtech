73a5fc53f1fb7e83a96f78700df35438ee7e4d56
BACH VAN THANH HUY <67851389+ThanhHuy999@users.noreply.github.com>
2023-05-10 13:14:08 +0700

    Update index1.php

--- index1.php
+++ index1.php
@@ -3,7 +3,7 @@
  * @author vinasupport.com
  */
 // connect to a database 
-$dbConn = pg_connect("host=<ec2-44-213-228-107.compute-1.amazonaws.com> port=<5432> dbname=<duge63lregjm7> user=<tsfdnzfevzaigp> password=<98638e683f0711f3d801aeb83587b7ecbe33617836980f9eb641d38442598395>");
+$dbConn = pg_connect("host='ec2-44-213-228-107.compute-1.amazonaws.com' port='5432' dbname='duge63lregjm7' user='tsfdnzfevzaigp' password='98638e683f0711f3d801aeb83587b7ecbe33617836980f9eb641d38442598395'");
 if (!$dbConn) {
     echo "An error occurred123.\n";
     exit;
