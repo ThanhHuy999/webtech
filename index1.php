904ad8cff9fb4b3ce07dfd264f78a910292ca96b
BACH VAN THANH HUY <67851389+ThanhHuy999@users.noreply.github.com>
2023-05-10 13:06:24 +0700

    Update index1.php

--- index1.php
+++ index1.php
@@ -3,13 +3,13 @@
  * @author vinasupport.com
  */
 // connect to a database 
-$dbConn = pg_connect("host="ec2-44-213-228-107.compute-1.amazonaws.com" port="5432" dbname="duge63lregjm7" user="tsfdnzfevzaigp" password="98638e683f0711f3d801aeb83587b7ecbe33617836980f9eb641d38442598395"");
+$dbConn = pg_connect("host=<ec2-44-213-228-107.compute-1.amazonaws.com> port=<5432> dbname=<duge63lregjm7> user=<tsfdnzfevzaigp> password=<98638e683f0711f3d801aeb83587b7ecbe33617836980f9eb641d38442598395>");
 if (!$dbConn) {
     echo "An error occurred.\n";
     exit;
 }
 // Query data
-$result = pg_query($dbConn, 'SELECT * FROM BAI_BAO');
+$result = pg_query($dbConn, 'SELECT * FROM BAI_BAO;');
 if (!$result) {
     echo "An error occurred.\n";
     exit;
