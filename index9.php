226dcf1bb96f883d5256ce8fdb1f9b724a673a9d
huyb1809579@student.ctu.edu.vn <67851389+ThanhHuy999@users.noreply.github.com>
2023-08-13 17:09:48 +0700

    index9.php

--- index9.php
+++ index9.php
@@ -1,8 +1,62 @@
-<?php
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = "Mickey Mouse\n";
-fwrite($myfile, $txt);
-$txt = "Minnie Mouse\n";
-fwrite($myfile, $txt);
-fclose($myfile);
+<?php>
+<html>
+<head>
+<meta charset="utf-8">
+	
+<title>Trang chủ | Chia sẻ kiến thức công nghệ</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body leftmargin=150px rightmargin=150px background-color="yellow">
+	<marquee bgcolor="#DC1D44">Để sử dụng website tốt nhất. Vui lòng truy cập trên trình duyệt máy tính. Trân trọng./.</marquee>
+	<header>
+	  <h1>Chia sẻ kiến thức công nghệ, kỹ thuật </h1>
+		<h3 align="right"><i><u>Công nghệ cho cuộc sống!</u></i></h3>
+	</header>
+	
+	
+<!--	Thanh menu -->
+	<nav>
+		<ul>
+			<li><a href"#"> Trang chủ </a></li>
+		    <li class="dropdown">
+				<a href="javascript:void(0)" class="dropbtn">Công nghệ thông tin</a>
+				<div class="dropdown-content">
+					 <a href="#">Thủ thuật máy tính</a>
+					 <a href="#">Heroku</a>
+					 <a href="#">Photoshop</a>
+					<a href="#">Dreamweaver</a>
+           		 </div>
+			</li>
+				 
+			<li><a href"#"> Cơ điện tử </a></li>
+			<li><a href"#"> Tự động hóa </a></li>
+			<li><a href"#"> Về chúng tôi </a></li>
+	
+		
+		</ul>
+	</nav>
+	
+	<main>
+		<section align="center">
+			<br>
+			<form action="/action_page.php">
+
+			<input type="text" id="fname" name="fname" placeholder="Bạn đang tìm gì ?">
+			<input type="submit" value="Tìm kiếm">
+			</form>
+		</section>
+		
+		<section align="right">
+			<a>Tiếng Việt/English</a>
+		</section>
+	</main>
+	
+	<h2 style="color:red;" align="center"><u>VỀ CHÚNG TÔI</u></h2>
+	<hr>
+	<p>Website kiến thức công nghệ được thành lập ngày 12/08/2023 với mục tiêu chia sẻ các kiến thức công nghệ tiên tiến của thế giới đến mọi người. Nếu bạn có chung niềm đam mê với chúng tôi, vui lòng gửi email về địa chỉ admin@kienthuccongnghe.com.vn để đăng ký thông tin cùng chia sẻ các kiến thức công nghệ mà bạn biết đến mọi người.</p>
+	<p bgcolor="red" align="center">LƯU Ý</p>
+	<p>Các bài viết cộng tác viên viết tùy theo số lượng lượt xem bài viết sẽ được chi trả tiền nhuận bút vào ngày 1 tây hằng tháng qua tài khoản ngân hàng bạn đăng ký với chúng tôi. </p>
+</body>
+</html>
 ?>
