133f8f5345cd02f66b375d70dae3648e2ad56e35
huyb1809579@student.ctu.edu.vn <67851389+ThanhHuy999@users.noreply.github.com>
2024-07-06 17:37:47 +0700

    Update thunghiem.php

--- thunghiem.php
+++ thunghiem.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body background-color="red" >
-    <h1>ĐĂNG NHẬP TÀI KHOẢN</h1>
+<body style="background-color:red;" >
+    <h1 text="white">ĐĂNG NHẬP TÀI KHOẢN</h1>
     <form action="">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required><br><br>
