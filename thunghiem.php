37ccf5fb1e35bdd5628c551a8ebe2482aa56d91c
huyb1809579@student.ctu.edu.vn <67851389+ThanhHuy999@users.noreply.github.com>
2024-07-13 21:24:06 +0700

    index.html

--- thunghiem.php
+++ thunghiem.php
@@ -6,12 +6,17 @@
     <title>Document</title>
 </head>
 <body style="background-color:red;" >
+    <div>
+
     <h1 text="white" align="center">ĐĂNG NHẬP TÀI KHOẢN</h1>
     <form action="">
         <label align="center" for="username">Username:</label>
         <input align="center" type="text" id="username" name="username" required><br><br>
         <label align="center" for="password">Password:</label>
         <input align="center" type="password" id="password" name="password" required><br><br>
+
     </form>
+
+    </div>
 </body>
 </html>
